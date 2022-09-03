0c709f17df0cd5e2e0946e50e40127a613fe8b77
Aleksandr Petushenko <moonbl4@mail.ru>
2022-09-04 02:40:11 +0300

    PuzeekBot init

---
+++ src/main/java/com/moon/puzeekbot/PuzeekBotApplication.java
@@ -0,0 +1,13 @@
+package com.moon.puzeekbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuzeekBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PuzeekBotApplication.class, args);
+    }
+
+}
